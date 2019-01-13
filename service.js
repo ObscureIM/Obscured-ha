fae5bcd7c28488f185dd3b0c80223518721ce56c
Syahrul <pauscrypto@gmail.com>
2019-01-13 11:04:13 +0800

    Enabled cors support

--- service.js
+++ service.js
@@ -24,7 +24,7 @@ var daemon = new TurtleCoind({
   dataDir: '~/.Obsidian', // Where do you store your blockchain?
   logfile: '/.Obsidian.log', //Where to store log file
   testnet: false, // Use the testnet?
-  enableCors: false, // Enable CORS support for the domain in this value
+  enableCors: True, // Enable CORS support for the domain in this value
   enableBlockExplorer: true, // Enable the block explorer
   loadCheckpoints: false, // If set to a path to a file, will supply that file to the daemon if it exists.
   rpcBindIp: '0.0.0.0', // What IP to bind the RPC server to
